f1a95576b38b397bbddb8d3993f3b32ab8112b11
Felipe Oliveira <feliveira8@gmail.com>
2025-09-02 14:37:30 -0300

    :wrench: chore: configure eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignores: ['node_modules', 'dist', 'coverage'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
